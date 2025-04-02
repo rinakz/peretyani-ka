a9eabd99e2af3f80136e85ae4f393dddba8b4a74
Катя <katya@bigmac.local>
2025-04-02 18:50:32 +0300

    create app

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <main className="bg-best_price-background lg:px-52 px-8 py-20">
+      пупупу{" "}
+    </main>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      best_price: {
+        background: "#F6F5FA",
+        white: "white",
+        black: "#2A2C36",
+        gray: "#898B94",
+        primary: "#8BB900",
+        red: "#F33939",
+      },
+    },
+    extend: {},
+  },
+  plugins: [],
+};
